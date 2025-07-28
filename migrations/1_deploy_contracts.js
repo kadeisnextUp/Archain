9eda676bd189e1ed4e330b665223901bfb364634
Kade <sawyerkd@mail.uc.edu>
2025-07-28 15:16:11 -0400

    trying to launch the token but running into deploy errors

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -2,6 +2,7 @@
 const Archain = artifacts.require("Archain");
 
 module.exports = function (deployer) {
-  deployer.deploy(Archain);
+    const initialSupply = web3.utils.toWei("1000000", "ether"); // 1 million ARCH
+    deployer.deploy(Archain, initialSupply);
 };
 // This migration script deploys the Archain contract to the blockchain.
